8944f0924c6b952aae7e11ee90a326d7e0227977
Jean Pool <114825140+Shinna7@users.noreply.github.com>
2023-05-04 13:08:39 -0500

    Actuañización40
    Actuñización

--- javascript/java.js
+++ javascript/java.js
@@ -1,11 +1,19 @@
+let header = document.getElementById('header');
 
-window.addEventListener("scroll",function(){
-    const header = document.querySelector('header');
-    header.classList.toggle('sticky',window.scrollY > 0);
-});
+// let nube1 = document.getElementById('nube1');
+// let fondo_transparente = document.getElementById('fondo_transparente');
+// let nube2 = document.getElementById('nube2');
+// let fondo = document.getElementById('fondo');
+// let viento = document.getElementById('viento');
+// let persona = document.getElementById('persona');
+// let slogan = document.getElementById('slogan');
 
-const navigation = document.querySelector('nav');
-document.querySelector('.toggle').onclick = function(){
-    this.classList.toggle('active');
-    navigation.classList.toggle('active');
-}
+let prota1 = document.getElementById('prota1');
+let prota2 = document.getElementById('prota2');
+
+window.addEventListener('scroll', function() {
+    let value = window.scrollY;
+
+    prota2.style.marginLeft = value * 0.7 + 'px';
+    prota1.style.left = value * -0.7 + 'px';
+})
