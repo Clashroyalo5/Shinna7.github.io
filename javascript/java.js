de1cedf90af756f76d95e0933325f746a894ebbb
Jean Pool <114825140+Shinna7@users.noreply.github.com>
2023-05-05 10:41:31 -0500

    .

--- javascript/java.js
+++ javascript/java.js
@@ -1,12 +1,5 @@
 let header = document.getElementById('header');
 
-// let nube1 = document.getElementById('nube1');
-// let fondo_transparente = document.getElementById('fondo_transparente');
-// let nube2 = document.getElementById('nube2');
-// let fondo = document.getElementById('fondo');
-// let viento = document.getElementById('viento');
-// let persona = document.getElementById('persona');
-// let slogan = document.getElementById('slogan');
 
 let prota1 = document.getElementById('prota1');
 let prota2 = document.getElementById('prota2');
